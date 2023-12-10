30f4adbe8c61b697d0c37947bd3e0e1be6bb43a7
Atul Mishra <139470533+atulmishraaaa@users.noreply.github.com>
2023-12-10 19:33:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+function functionName() {
+  console.log(arguments.callee.name);
+}
+
+alert('functionName');
+function Acciojob(){
+	console.log(arguments.callee.name);
+}
+alert("Acciojob");
